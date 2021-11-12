c2a653393cc64275fb291727d2231feeb8380fe0
Vinay <Vinay@DESKTOP-I8D9V6P>
2021-11-12 12:04:26 +0530

    hai

---
+++ SDET24/src/main/java/com/vtiger/genericLibrary/javaUtility.java
@@ -0,0 +1,22 @@
+package com.vtiger.genericLibrary;
+
+import java.util.Random;
+/**
+ * 
+ * @author Vinay
+ *
+ */
+public class javaUtility {
+	/**
+	 * 
+	 * @return the random value
+	 */
+	public String randomInt() {
+		Random r= new Random();
+		int random = r.nextInt(1000);
+		return ""+random;
+		
+	}
+
+}
+ 
---
+++ SDET24/src/test/java/practies/sumne.java
@@ -0,0 +1,12 @@
+package practies;
+
+import org.testng.annotations.Test;
+
+public class sumne {
+	
+	@Test
+	public void sumneCreateaTest() {
+		System.out.println("sumne test madtirodu");
+	}
+	
+}
